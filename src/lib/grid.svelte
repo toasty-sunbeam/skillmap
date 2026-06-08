c8eee5959d8ddb61a2492900e0cf81455c299330
James Harris <JHarris@gtindependence.com>
2026-06-08 10:44:55 -0400

    Skill node

--- src/lib/grid.svelte
+++ src/lib/grid.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
 	import Icon from '@iconify/svelte';
+	import Node from './node.svelte';
 </script>
 
 <div class="h-full w-full bg-[url(grid.png)] text-white"> <!-- Background image -->
 	<div>
 		<Icon icon="ph:graduation-cap-bold" />
 		<Icon icon="game-icons:anvil" />
+		<Node />
 	</div>
 	<svg></svg> <!-- Edges -->
 </div>
---
+++ src/lib/node.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	let points = $state(0);
+	let maxPoints = $state(1);
+	let icon = $state();
+	let name = $state('Skill 1');
+
+	function handleClick() {
+		if (points < maxPoints) {
+			points++;
+		}
+	}
+	function handleRightClick(event: MouseEvent) {
+		if (points > 0) {
+			points--;
+		}
+		event.preventDefault();
+	}
+</script>
+
+<button onclick={handleClick} oncontextmenu={handleRightClick}>
+	<div>{name}</div>
+	<div>{points}/{maxPoints}</div>
+</button>
